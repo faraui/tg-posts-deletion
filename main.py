394eed59efb4559100a5cc996da86515f3b2cf82
Ruslan Abbas Al Faraui <rusya.abbas@gmail.com>
2024-05-31 16:46:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = pyrogram.Client(
     "main",
     api_id=config.api_id, api_hash=config.api_hash
 )
-# bot = pyrogram.Client(
-#    "main_bot",
-#    api_id=config.api_id, api_hash=config.api_hash,
-#    bot_token=config.bot_token
-# )
+bot = pyrogram.Client(
+   "main_bot",
+   api_id=config.api_id, api_hash=config.api_hash,
+   bot_token=config.bot_token
+)
 
 class Shreder:
     def __init__(self, target):
